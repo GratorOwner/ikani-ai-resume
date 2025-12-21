4234bdc845b9149cfd3452e6009a87b3eaa0b30d
GratorOwner <153442558+GratorOwner@users.noreply.github.com>
2025-12-21 22:40:21 +0000

    Adding MUI and some buttons to the lander.

--- ai-resume/src/components/SplashLander.tsx
+++ ai-resume/src/components/SplashLander.tsx
@@ -1,5 +1,6 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
 
 
 
@@ -58,7 +59,16 @@ export default function SplashLander() {
                     background: "lightblue",
                 }}
             >
-                <h2>Congrats! We now have the transition from splash to lander.</h2>
+                <h2>Philip Ikani's AI agent based resume.</h2>
+                <p>This isn't just any old boring, flat PDF resume. This is a interactive resume,
+                    leveraging AI to provide a more in depth view of Philip's skills and work history. 
+                    No more scanning endlessly boring dull text and unfamiliar formatting. Jump straight 
+                    to the agent or scroll the page for an interactive experience!
+                </p>
+                <Button>Chat with my AI agent</Button>
+                <Button>Check out an interactive work history summary</Button>
+                <Button>Learn about this exciting new type of resume</Button>
+
             </motion.div>
             )}
         </div>
